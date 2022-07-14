e84c4d4bdaaf116242349a48060459c22f21defb
ChichebeDarlington <73057314+ChichebeDarlington@users.noreply.github.com>
2022-07-14 06:10:28 -0700

    create-react-app

---
+++ amamerce/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <a href="#">Amamerce</a>
+      </header>
+      <main>product list</main>
+    </div>
+  );
+}
+
+export default App;
